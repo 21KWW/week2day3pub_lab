4c690bbf91e07559dbece7a00bc2e48e716dced3
Keith Wyse <kwyse3436@gmail.com>
2021-10-27 11:12:02 +0100

    added and passed pubs has drinks test

--- src/pub.py
+++ src/pub.py
@@ -1,5 +1,6 @@
 class Pub:
-    def __init__(self, init_name, init_cash):
+    def __init__(self, init_name, init_cash, init_drinks):
         self.name = init_name
         self.cash = init_cash
+        self.drinks = init_drinks
 
--- tests/pub_test.py
+++ tests/pub_test.py
@@ -6,10 +6,16 @@ from src.drinks import Drink
 
 class TestPub(unittest.TestCase):
     def setUp(self):
-        self.pub = Pub("The Prancing Pony", 100)
+        drink1 = Drink("Beer", 5)
+        drink2 = Drink("Wine", 4)
+        drinks = [drink1, drink2]
+        self.pub = Pub("The Prancing Pony", 100, drinks)
 
     def test_pub_has_name(self):
         self.assertEqual("The Prancing Pony", self.pub.name)
 
     def test_pub_has_till(self):
         self.assertEqual(100, self.pub.cash)
+
+    def test_pub_has_drinks(self):
+        self.assertEqual(2, len(self.pub.drinks))
